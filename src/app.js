c22145fb491427582ec352debc5ab03f4b41f558
Raman Singh Chauhan <ramansinghchauhan787@gmail.com>
2025-11-11 15:58:03 +0545

    wrapping async handler in a file

--- src/app.js
+++ src/app.js
@@ -1,2 +1,24 @@
-import express from 'express'
-const app  = express();
+import express from "express";
+const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true, // credentials allows  sending cookies or token  especially for verification
+  })
+);
+app.use(express.json({ limit: "10kb" }));
+//It enables Express to automatically parse JSON data that comes in an HTTP request body.
+
+// So yes — it lets your app directly access JSON, but through req.body
+
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
+
+app.use(express.static("public")); // this line means Hey Express, whenever someone requests a file that exists in this folder, just send it directly
+app.use(cookieParser())
---
+++ src/utils/asynchandler.js
@@ -0,0 +1,33 @@
+// const asynchandler = ()=>{
+//     ()=>{}
+// }
+// this fucntion cann be written as 
+
+
+// export const asynchandler =(func)=>{
+// return async (req,res,next)=>{
+// try {
+//     await func(req,res,next)
+// } catch (error) {
+//     res.status(error.code || 500).json({
+//         success : false,
+//         message: error.message
+//     })  //res.status(...) → sets the HTTP status code for the response
+// }
+
+// }
+// }
+
+
+// ANOTHER WWAY OF THIS CODE
+
+
+export const asynchandler = (fn)=> {
+    return (req,res,next)=>{
+        Promise.resolve(fn(req,res,next)).catch((error)=>{
+            next(error)
+        })
+
+    }
+
+}
