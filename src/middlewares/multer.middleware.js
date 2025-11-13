5a99b92055b7564d1e394edba8941f63643904ea
Raman Singh Chauhan <ramansinghchauhan787@gmail.com>
2025-11-13 12:14:01 +0545

    handling multer

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,22 @@
+import multer from "multer";
+
+
+// 1. first handle storage then go for uploaad   and handle routes
+//disk storage
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/tmp");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.originalname + "-" + uniqueSuffix);
+  },
+});
+
+export const upload = multer({ storage: storage });
+
+// app.post('/profile', upload.single('avatar'), function (req, res, next) {
+//   // req.file is the `avatar` file
+//   // req.body will hold the text fields, if there were any
+// })
