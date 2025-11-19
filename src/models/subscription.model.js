7bfe56a4693c908b2d11a31251c1697b7dccf06f
Raman Singh Chauhan <ramansinghchauhan787@gmail.com>
2025-11-19 17:42:05 +0545

    Define subscription schema with unique index for subscriber and channel

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,4 +1,34 @@
 import mongoose from "mongoose";
 
-const  subscriptionModel =  new mongoose.Schema({},{timestamps: true})
-export const Subscription  = mongoose.model('Subscription' ,subscriptionModel )
+const  subscriptionSchema =  new mongoose.Schema({
+subscriber  : {
+    type : mongoose.Schema.Types.ObjectId,
+    ref : User,
+    required : true
+},
+
+channel : {
+    type : mongoose.Schema.Types.ObjectId,
+    ref : user ,
+    required : true
+}
+
+
+},{timestamps: true})
+
+
+
+
+
+subscriptionSchema.index({subscriber:1, channel : 1},{unique: true})
+/*
+
+This tells MongoDB:
+
+For every pair of (subscriber, creator/channel), there must be NO duplicates.
+One subscriber can subscribe to the same creator only once.
+*/
+
+
+
+export const Subscription  = mongoose.model('Subscription' ,subscriptionSchema )
