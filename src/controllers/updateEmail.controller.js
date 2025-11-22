860236ed2e77f46711c6fbf3099b293c09fffba4
Raman Singh Chauhan <ramansinghchauhan787@gmail.com>
2025-11-22 17:25:33 +0545

    Implement updateEmail controller with validation and error handling

---
+++ src/controllers/updateEmail.controller.js
@@ -0,0 +1,53 @@
+import { User } from "../models/user.model.js";
+import ApiResponse from "../utils/apiResponse.js";
+import asynchandler from "../utils/asynchandler.js";
+import ApiError from "../utils/apiError.js";
+
+export const updateEmail = asynchandler(async(req,res)=>{
+//1.new email
+//2.if not entered  throw error
+//3.check format
+//4.find existing email
+//6.save in db
+//7.return resp
+try {
+    const {newEmail} = req.body ;
+    if (!newEmail || newEmail.trim() === "") {
+        throw new ApiError(400,"Email is required!!");
+        
+        
+    }
+    
+    // Validate proper email format
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(newEmail)) {
+        throw new ApiError(400, "Invalid email format");
+      }
+     // if entered email already exists
+    
+    const existingUser =  await User.findOne({email : newEmail});
+    if (existingUser) {
+        throw new ApiError(400,"email already exists try another email");
+        
+    }
+    const user = await User.findByIdAndUpdate(req.user._id,{email : newEmail},{new : true , select: "-password -refreshToken"})
+   
+    if (!user) {
+        throw new ApiError(404,"User not found");
+
+        
+        
+    }
+    
+    return res.status(200).json(new ApiResponse(200,{user},"Email updated successfully"))
+    
+    
+    
+} catch (error) {
+    console.log("Failed to update email : ",error)
+    throw new ApiError(500,"Failed while updating Email");
+    
+}
+
+
+})
