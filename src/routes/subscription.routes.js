178065504a37799835f00bac42ba4ea252423a71
Raman Singh Chauhan <ramansinghchauhan787@gmail.com>
2025-12-02 13:13:19 +0545

    Add subscription and tweet routes with JWT verification middleware

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,2 +1,12 @@
 import { Router } from "express";
 import {toggleSubscription,getUserChannelSubscribers} from '../controllers/subscription.controller.js'
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+const router = Router()
+
+router.use(verifyJWT)
+
+
+router.route("/c/:channelID").post(toggleSubscription)
+
+router.route('/u/:subscriberID').get(getUserChannelSubscribers)
+
